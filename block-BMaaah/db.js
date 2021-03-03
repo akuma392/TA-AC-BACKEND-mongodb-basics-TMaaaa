aa68fd123fa27138dcd53ce67b9f44199b865cd2
Abhishek Kumar <abhivision3@gmail.com>
2021-03-03 16:34:14 +0530

    assignment

---
+++ block-BMaaah/db.js
@@ -0,0 +1,18 @@
+// - title as string
+// - createdAt as date
+// - details as String
+// - author as nested object
+//   - author should have
+//     - name
+//     - email
+//     - age
+//     - example author: {name: 'abc', email: 'abc@gmail', age: 25}
+// - tags : Array of strings like ['html', 'css']
+
+let obj1 = {
+  title: 'Article1',
+  createdAt: '02/03/2021',
+  details: 'Drama',
+  author: { name: 'Abc', email: 'test@gmail.com', age: 25 },
+  tags: ['html', 'CSS'],
+};
